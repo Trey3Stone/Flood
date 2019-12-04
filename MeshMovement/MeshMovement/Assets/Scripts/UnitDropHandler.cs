b5584d38f5c1939255d36f39b3dc17f90ec2405d
Samir Ouafi <samir.ouafi@epitech.eu>
2019-12-03 16:36:45 -0800

    hud fixes

--- MeshMovement/MeshMovement/Assets/Scripts/UnitDropHandler.cs
+++ MeshMovement/MeshMovement/Assets/Scripts/UnitDropHandler.cs
@@ -1,5 +1,4 @@
-﻿using Dummiesman;
-using System.Collections;
+﻿using System.Collections;
 using System.Collections.Generic;
 using System.IO;
 using UnityEngine;
@@ -13,30 +12,7 @@ public class UnitDropHandler : MonoBehaviour, IDropHandler
 
         if (!RectTransformUtility.RectangleContainsScreenPoint(hotbar, Input.mousePosition))
         {
-            string objPath = string.Empty;
-            string error = string.Empty;
-            GameObject loadedObject;
-            GameObject gun;
-            Vector3 pos = new Vector3(1085, 923, 475);
-
-            loadedObject = new OBJLoader().Load("C:/Users/samspam/Flood/MeshMovement/MeshMovement/Assets/Models/Turret.obj");
-            loadedObject.transform.position = pos;
-            loadedObject.transform.rotation = transform.rotation = Quaternion.AngleAxis(180, Vector3.up); ;
-
-            gun = GameObject.Find("Turret/Gun_LOD0");
-            gun.AddComponent<MachineGunTurretBehaviour>();
+            // Drop/Create object
         }
-    }
-
-    // Start is called before the first frame update
-    void Start()
-    {
-        
-    }
-
-    // Update is called once per frame
-    void Update()
-    {
-        
     }
 }
